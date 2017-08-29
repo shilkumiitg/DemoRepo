8becb0f0505fc86abfc61f85f8f5bc433414f267
shilkumiitg <cooltulip.03@gmail.com>
2017-08-29 14:35:04 +0530

    added files

---
+++ src/main/java/com/shilp/github/branching/App.java
@@ -0,0 +1,17 @@
+package com.shilp.github.branching;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+<<<<<<< HEAD
+        System.out.println( "Wc to github");
+=======
+        System.out.println( "Hello Shilpa!" );
+>>>>>>> 3168ffacc1d728cf55e4a3115c0bd1301201dc99
+    }
+}
